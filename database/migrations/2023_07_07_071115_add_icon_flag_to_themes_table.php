e93ae42b4b03ac5bc45fb3a4471564a45d822163
Julian <padliyulian@gmail.com>
2023-07-07 14:18:27 +0700

    adding icon flag on themes

---
+++ database/migrations/2023_07_07_071115_add_icon_flag_to_themes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('themes', function (Blueprint $table) {
+            $table->tinyInteger('icon')->default(2)->after('group_id');
+        });
+
+        DB::table('themes')->whereIn('id', [1,2,3])->update(['icon' => 1]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('themes', function (Blueprint $table) {
+            $table->dropColumn('icon');
+        });
+    }
+};

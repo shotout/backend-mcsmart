fff4859eb6fb50195385e9bc3a15481db3211c10
shotout <hevaldo66@gmail.com>
2023-09-04 16:19:06 +0700

    Update profile

--- app/Http/Controllers/Api/v1/UserProfileController.php
+++ app/Http/Controllers/Api/v1/UserProfileController.php
@@ -8,6 +8,13 @@ use App\Jobs\GenerateTimer;
 use Illuminate\Http\Request;
 use App\Jobs\GenerateTimerAds;
 use App\Http\Controllers\Controller;
+use App\Models\Collection;
+use App\Models\PastQuote;
+use App\Models\Subscription;
+use App\Models\UserCategory;
+use App\Models\UserQuote;
+use App\Models\UserRepeat;
+use App\Models\UserTheme;
 
 class UserProfileController extends Controller
 {
@@ -54,6 +61,62 @@ class UserProfileController extends Controller
         if ($request->has('commit_goal') && $request->commit_goal != '') {
             $user->commit_goal = $request->commit_goal;
             $user->update();
+
+            //reset subscription registered device
+            $subscription = Subscription::where('user_id', $user->id)->first();
+            if ($subscription) {
+                $subscription->plan_id = 1;
+                $subscription->type = 1;
+                $subscription->update();
+            }
+
+            //reset repeat list
+            $repeat = UserRepeat::where('user_id', $user->id)->get();
+            if($repeat) {                
+               foreach($repeat as $r) {
+                   $r->delete();
+               }
+            }
+
+            //reset like list
+            $liked = UserQuote::where('user_id', $user->id)->get();
+            if($liked) {                
+               foreach($liked as $l) {
+                   $l->delete();
+               }
+            }
+
+            //reset category
+            $categories = UserCategory::where('user_id', $user->id)->whereNotIn('category_id',[1,2])->get();
+            if($categories) {                
+               foreach($categories as $c) {
+                   $c->delete();
+               }
+            }
+
+            //reset collection
+            $collections = Collection::where('user_id', $user->id)->get();
+            if($collections) {                
+               foreach($collections as $cl) {
+                   $cl->delete();
+               }
+            }
+
+            //reset past facts
+            $facts = PastQuote::where('user_id', $user->id)->get();
+            if($facts) {                
+               foreach($facts as $f) {
+                   $f->delete();
+               }
+            }
+
+            //reset to random theme
+            $themes = UserTheme::where('user_id', $user->id)->first();
+            if($themes) {                
+                $themes->theme_id = 1;
+                $themes->update();
+               }
+            
         }
 
         // schedule reminder ------------
